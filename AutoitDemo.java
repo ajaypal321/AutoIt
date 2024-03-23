fb5c6a40c22325e0e03125e4f431cba4f383de11
ajaypal321 <ajaypal.iitb23@gmail.com>
2024-03-23 14:01:18 +0530

    autoit

---
+++ AutoitDemo.java
@@ -0,0 +1,35 @@
+package com.autoit;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AutoitDemo {
+	public static WebDriver driver;
+	public static void main(String[] args) throws InterruptedException, IOException  {
+		System.setProperty("Webdriver-http-factory","jdk-http-client");
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		//System.setProperty("webdriver.chrome.driver","D://Driver//chromedriver.exe");	
+		//create instance of chromedriver
+		//**WebDriver driver=  new ChromeDriver();
+		
+			//	open the website
+		driver.get("https://www.filemail.com/share/upload-file");
+     //driver.get("https://altoconvertpdftojpg.com/");
+     driver.findElement(By.id("addBtn")).click();
+     //provide the path of autoit.exe file
+     
+    // String scriptpath="D:\\AutoIt\\AutoitFirst.exe";
+   Thread.sleep(2000);
+    Runtime.getRuntime().exec("C:/Users/ajay/Documents/auto/uploadfile1.exe");
+//     ProcessBuilder bulider= new ProcessBuilder(scriptpath);
+//     bulider.start();
+	}
+
+}
